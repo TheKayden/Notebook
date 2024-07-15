9b37cac811cc1a374dbcc0c66de7fff6f63dc089
Siva Inampudi <inampude@gmail.com>
2024-07-15 16:01:52 -0400

    first program

---
+++ dev/shiva/java/static_import/StaticImportExample1.java
@@ -0,0 +1,7 @@
+package dev.shiva.java.static_import;
+
+public class StaticImportExample1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Static Import");
+    }
+}
